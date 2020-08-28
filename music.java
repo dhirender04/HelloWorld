4144dde6ca64418bb0d34be5b37a955c91d2689b
dhirender04 <kumardhirender04@gmail.com>
2020-08-28 10:22:28 +0530

    This is my second commit

---
+++ music.java
@@ -0,0 +1,13 @@
+class music{
+public static void  main(Strings args[]){
+    Scanner sc=new Scanner() ;
+
+  System.out.println("sum of two number is "+sumfun());
+}
+    public void sumfun(){
+        int a = sc.nextInt();
+        int b=sc.nextInt();
+        int sum;
+        sum=a+b;
+    }
+}
